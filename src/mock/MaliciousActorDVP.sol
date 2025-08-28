9fc710c3eae7f60a2fa811426a6fa07b3b90e6f1
Kevin <kevin@pv0.one>
2025-08-28 16:21:27 +0100

    Upgrade solidity 0.8.30 and set via IR as default

--- src/mock/MaliciousActorDVP.sol
+++ src/mock/MaliciousActorDVP.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.30;
 
 import {IDeliveryVersusPaymentV1} from "./IDeliveryVersusPaymentV1.sol";
 
@@ -23,7 +23,7 @@ contract MaliciousActorDVP {
     dvp = _dvp;
     reentrancyMode = ReentrancyMode.NoReentrancy;
   }
-  
+
   function setTargetSettlementId(uint256 _targetSettlementId) external {
     targetSettlementId = _targetSettlementId;
   }
